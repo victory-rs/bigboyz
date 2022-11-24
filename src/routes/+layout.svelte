63ddecd925349a34c0740e05ec26923b10da87b4
Milos Spasojevic <milos.s.pfc@gmail.com>
2022-11-24 13:05:38 +0100

    font fallback, seo meta-data

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,13 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>BigBoyz</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="description" content="BigBoyz. Sports, Food & Good Mood. Južni bulevar 42, Beograd" />
+	<meta
+		name="keywords"
+		content="BigBoyz, Bar, Restoran, Restaurant, Sports, Food, Good Mood, Južni bulevar 42, Beograd"
+	/>
+</svelte:head>
 <slot />
