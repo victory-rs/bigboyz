5c6a0298e0e872c10c5a813fb493e65650f2388d
therealdzo <127553289+therealdzo@users.noreply.github.com>
2023-03-10 22:40:51 +0100

    Dzo je developer! (#1)
    * Delete BB-karta-pića_02.pdf
    
    * Delete BB-jelovnik_01.pdf
    
    * Add files via upload
    
    Nove verzije jelovnika i picovnika
    
    * Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <div class="w-screen min-h-screen grid justify-center items-center bg-[#718b74] text-[#e0d0a7]">
 	<div class="w-full flex flex-col gap-8 justify-center items-center">
 		<img alt="BigBoyz logo" src="/logo.png" class="logo w-full" width="1080" height="1080" />
-		<a href="BB-jelovnik_01.pdf" target="blank" class="url underline">jelovnik</a>
-		<a href="BB-karta-pića_02.pdf" target="blank" class="url underline">karta pića</a>
+		<a href="BB-JELOVNIK-OK.pdf" target="blank" class="url underline">jelovnik</a>
+		<a href="BB-KARTA_PIĆA preview.pdf" target="blank" class="url underline">karta pića</a>
 		<p class="text-center text-lg">Južni bulevar 42, Beograd</p>
 		<a href="https://www.instagram.com/bigboyz_bg" class="w-6 h-6"
 			><img alt="instagram" class="instagram" src="/instagram.png" width="24" height="24" /></a
